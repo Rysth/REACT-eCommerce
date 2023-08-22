3a363a3b2bb6a62a51aaee8a852921749790c3f5
Rysth <johnpalacios.t@gmail.com>
2023-08-22 16:45:23 -0500

    Created the Hero component with background.

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Hero.css';
+import HeroImage from '../../assets/images/hero.png';
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="container">
+        <div className="hero-left">
+          <h2 className="hero-title">Discover Most Suitable Watches</h2>
+          <p className="hero-text">
+            Find the best, reliable, and cheap smart watches here. We focus on product quality. Here
+            you can find smart watches of almost all brands. So why you are waiting? Just order now!
+          </p>
+        </div>
+        <div className="hero-right">
+          <img src={HeroImage} alt="Hero watch" className="hero-image" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
