0b665a23c12a8cec4a9ed4109146ddd6f6475d12
Rysth <johnpalacios.t@gmail.com>
2023-08-22 16:58:00 -0500

    Finished the mobile version of the Hero.

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,6 +12,16 @@ function Hero() {
             Find the best, reliable, and cheap smart watches here. We focus on product quality. Here
             you can find smart watches of almost all brands. So why you are waiting? Just order now!
           </p>
+          <form action="" className="form">
+            <div className="form-group">
+              <label htmlFor="searchBar" className="form-label search">
+                <input className="form-input" type="text" placeholder="Find the best brands" />
+                <button className="form-button" type="button">
+                  Search
+                </button>
+              </label>
+            </div>
+          </form>
         </div>
         <div className="hero-right">
           <img src={HeroImage} alt="Hero watch" className="hero-image" />
