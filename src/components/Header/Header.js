fdcf8accc7c7e2007b453d2653809cc0a99c3202
Rysth <johnpalacios.t@gmail.com>
2023-08-22 15:37:32 -0500

    Add functionality to handle the mobile menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
 function Header() {
+  /* Handle de Mobile Menu */
+  const [open, setOpen] = useState(false);
+  const handleMobileMenu = () => setOpen((prevOpen) => !prevOpen);
+
   return (
     <div className="header">
       <div className="container">
@@ -10,10 +14,10 @@ function Header() {
           <h1 className="header-title">SmartCommerce</h1>
         </header>
         <div className="header-middle">
-          <button className="header-button" type="button">
-            <i className="fa-solid fa-bars" />
+          <button className="header-button" type="button" onClick={handleMobileMenu}>
+            <i className={open ? 'fa-solid fa-close color-white' : 'fa-solid fa-bars'} />
           </button>
-          <nav className="header-nav d-flex">
+          <nav className={open ? 'header-nav d-flex' : 'header-nav d-none'}>
             <a className="header-link" href="/">
               Home
             </a>
