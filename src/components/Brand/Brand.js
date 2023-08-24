1b916107a8084bd0c2cf3c2d01b5228d8529ea2d
Rysth <johnpalacios.t@gmail.com>
2023-08-24 17:49:45 -0500

    Created the brands dinamycally and installed the Carousel functionality.

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import BrandImage from '../../assets/images/brands/preview-1.png';
+import PropType from 'prop-types';
 import './Brand.css';
 
-function Brand() {
+function Brand({ imagePath, brand, text }) {
   return (
     <div className="brand">
       <div className="brand-header">
-        <img className="brand-image" src={BrandImage} alt="" />
+        <img className="brand-image" src={imagePath} alt="" />
       </div>
       <div className="brand-body">
-        <h3 className="brand-name">Apple</h3>
-        <p className="brand-text">
-          Apple is one of the most famous smart watches providing company.
-        </p>
+        <h3 className="brand-name">{brand}</h3>
+        <p className="brand-text">{text}</p>
       </div>
     </div>
   );
 }
+
+Brand.propTypes = {
+  imagePath: PropType.string.isRequired,
+  brand: PropType.string.isRequired,
+  text: PropType.string.isRequired,
+};
 
 export default Brand;
---
+++ src/components/Brand/BrandImages.js
@@ -0,0 +1,26 @@
+import BrandOne from '../../assets/images/brands/preview-1.png';
+import BrandTwo from '../../assets/images/brands/preview-2.png';
+import BrandThree from '../../assets/images/brands/preview-3.png';
+
+const brandsArray = [
+  {
+    ID: 'item-1',
+    imagePath: BrandOne,
+    brand: 'Apple',
+    text: 'Apple is one of the most famous smart watches providing company.',
+  },
+  {
+    ID: 'item-2',
+    imagePath: BrandTwo,
+    brand: 'Xiaomi',
+    text: 'Xiaomi smart watches are produced by MI company.',
+  },
+  {
+    ID: 'item-3',
+    imagePath: BrandThree,
+    brand: 'FitBit',
+    text: 'FitBit smart watches are best for there health and fitness features.',
+  },
+];
+
+export default brandsArray;
