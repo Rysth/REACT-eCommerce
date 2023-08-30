f344e713c9d947ced8af9989d21acc1b6ad18ab1
Rysth <johnpalacios.t@gmail.com>
2023-08-30 13:05:11 -0500

    Update Preview css handling correctly the items.

--- src/pages/Preview/Preview.js
+++ src/pages/Preview/Preview.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import ImageZoom from 'react-image-zooom';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { NotificationManager } from 'react-notifications';
 import { useNavigate, useParams } from 'react-router-dom';
-import ImagePreview from '../../assets/images/products/product-3.png';
 import { fetchSingleProduct, cartActions } from '../../redux/Cart/CartSlice';
+import { fetchProductByID } from '../../redux/Products/ProductsSlice';
 import './Preview.css';
 
 function Preview() {
@@ -12,7 +11,6 @@ function Preview() {
   const handleNavigation = () => {
     navigator('/');
   };
-
   const params = useParams();
   const dispatch = useDispatch();
   const handleCartItems = () => {
@@ -20,15 +18,21 @@ function Preview() {
     dispatch(cartActions.incrementCounter());
     NotificationManager.success('Product Added', 'Successfull', 1000);
   };
+
+  const { productSelected } = useSelector((store) => store.products);
+
+  useEffect(() => {
+    dispatch(fetchProductByID(params.id));
+  }, [dispatch]);
+
   return (
     <div className="preview">
       <div className="preview-content container">
         <picture className="preview-picture">
-          <ImageZoom
+          <img
             className="preview-image"
-            src={ImagePreview}
-            alt="Product preview"
-            zoom="200"
+            src={productSelected && productSelected.image}
+            alt=""
           />
         </picture>
         <div className="preview-body">
@@ -42,19 +46,18 @@ function Preview() {
               Back
             </button>
             <hr className="preview-separator" />
-            <h2 className="preview-text title">Apple Watch III</h2>
+            <h2 className="preview-text title">
+              {productSelected && productSelected.title}
+            </h2>
           </header>
           <p className="preview-text parragraph">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas sequi
-            voluptatum commodi deleniti et similique atque earum delectus sed
-            inventore, reprehenderit aliquid illo cum eligendi praesentium
-            tempora neque. Illum, quaerat!
+            {productSelected && productSelected.description}
           </p>
           <h3 className="preview-text subtitle">Categories:</h3>
           <div className="preview-system">
-            <p className="preview-text category">Watches</p>
-            <p className="preview-text category">Apple</p>
-            <p className="preview-text category">Technology</p>
+            <p className="preview-text category">
+              {productSelected && productSelected.category}
+            </p>
           </div>
           <button
             type="button"
